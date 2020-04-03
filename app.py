cb55cac9642e3b7798a595b251e0b19d6340d683
Irene <lai860423@gmail.com>
2020-04-04 00:28:24 +0800

    print event

--- app.py
+++ app.py
@@ -36,8 +36,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
     print(event)
+    line_bot_api.reply_message(event.reply_token, message)
+    
 
 import os
 if __name__ == "__main__":
