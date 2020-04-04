af0b92605601a4fce37bd61ffa9878a0db070e92
Irene <lai860423@gmail.com>
2020-04-04 12:35:38 +0800

    copy message reply

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def callback():
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     print(event)
-    #line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, message)
     
 
 import os
