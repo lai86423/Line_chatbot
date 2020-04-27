68f9f698711b52e3edab92e98c9df472ed2db7c9
Irene <lai860423@gmail.com>
2020-04-27 08:27:47 +0800

    df iloc

--- app.py
+++ app.py
@@ -33,7 +33,9 @@ df = pd.read_csv('df.csv')
 # 以dataframe形式讀取資料
 #df = ws.get_as_df(index_colum=None, empty_value='', include_tailing_empty=False,numerize=False) # index 從 0 開始算
 print("DF=",df,type(df))
-question = df[0]
+print(df.iloc[:,0])
+print(df.ix[0])
+question = df.ix[0]
 optionA = df[1]
 optionB = df[2]
 optionC = df[3]
