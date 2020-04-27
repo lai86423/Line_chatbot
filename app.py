e9ff07857867cbc3198e031536e155150e6003f9
Irene <lai860423@gmail.com>
2020-04-27 08:52:14 +0800

    df loc

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ ws.export(filename='df')
 df = pd.read_csv('df.csv')
 # 以dataframe形式讀取資料
 #df = ws.get_as_df(index_colum=None, empty_value='', include_tailing_empty=False,numerize=False) # index 從 0 開始算
-print("Question",df.loc[:,['Question'])
+print("Question",df.loc[:,['Question']])
 print("DF",df.iloc[0])
 question = df.iloc[:,0]
 optionA = df.iloc[:,1]
