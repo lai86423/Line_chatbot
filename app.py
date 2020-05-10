9a9d37727a3da19d165cdc5a25dc4e114c1b8176
Irene <lai860423@gmail.com>
2020-05-10 15:35:11 +0800

    button test

--- app.py
+++ app.py
@@ -100,6 +100,32 @@ def handle_message(event):
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text='答對了！你真棒！'))
                 isAsked = False
                 index += 1
+        if event.message.text == "Buttons Template":
+            buttons_template = TemplateSendMessage(
+                alt_text='Buttons Template',
+                template=ButtonsTemplate(
+
+                    title = '這是ButtonsTemplate',
+                    text = 'ButtonsTemplate可以傳送text,uri',
+                    #thumbnail_image_url = '顯示在開頭的大圖片網址',
+                    actions = [
+                            MessageTemplateAction(
+                                label = 'ButtonsTemplate',
+                                text = 'ButtonsTemplate'
+                            ),
+                            URITemplateAction(
+                                label = 'VIDEO1',
+                                uri = 'https://john850512.wordpress.com/2019/02/02/python-line%E5%AE%A2%E8%A3%BD%E5%8C%96%E5%9B%9E%E8%A6%86%E8%A8%8A%E6%81%AF/'
+                            ),
+                            PostbackTemplateAction(
+                                label = 'postback',
+                                text = 'postback text',
+                                data = 'postback1'
+                            )
+                    ]
+                )
+            )
+    line_bot_api.reply_message(event.reply_token, buttons_template)
         #if user_id is None:
         #    user_id = event.source.user_id
         #    print("user_id =", user_id)       
