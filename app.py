6eb1e925b9ccc15d8ac8a04fc5c964ef7f1a758c
Irene <lai860423@gmail.com>
2020-05-13 12:55:51 +0800

    option ABC change to 123

--- app.py
+++ app.py
@@ -80,11 +80,11 @@ def handle_message(event):
     if event.message.type == 'text':
         if( isAsked == False ):            
             print(sheet["question"][index])
-            print("1:", sheet["optionA"][index], "\n2:", sheet["optionB"][index], "\n3:", sheet["optionC"][index],
-                    "\n4:", sheet["optionD"][index], "\n")
+            print("1:", sheet["option1"][index], "\n2:", sheet["option2"][index], "\n3:", sheet["option3"][index],
+                    "\n4:", sheet["option4"][index], "\n")
 
-            option = ("1:" + sheet["optionA"][index] + "\n2:" + sheet["optionB"][index] + "\n3:" + 
-                        sheet["optionC"][index] + "\n4:" + sheet["optionD"][index] + "\n")
+            option = ("1:" + sheet["option1"][index] + "\n2:" + sheet["option2"][index] + "\n3:" + 
+                        sheet["option3"][index] + "\n4:" + sheet["option4"][index] + "\n")
             question = sheet["question"][index]
             ask = question + "\n" + option  
             #line_bot_api.reply_message(event.reply_token, TextSendMessage(text=ask))  
