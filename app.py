f61a2c0d699c9b1dea25751bdd52d815dc81f45b
Lai-YingRu <lai860423@gmail.com>
2020-06-15 10:46:43 +0800

    2020/06/14 UI改一半 ui & feedback text & star point

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 from googletrans import Translator
 
-##出題小老師  import-----------------------------------------------
+##聽力測驗  import-----------------------------------------------
 import sys
 import datetime
 import pygsheets
@@ -21,41 +21,41 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('mIg76U+23oiAkDahsjUoK7ElbuYXzLDJcGXaEjaJIfZ+mMqOO3BvX+RlQIzx/Zu0Smy8W08i01F38xGDg6r/thlWLwGxRvcgExAucwMag8KPVAkBFfSLUvgcrxQS4HBzOGIBxoo+zRSJhOFoBEtCVQdB04t89/1O/w1cDnyilFU=')
 #Channel Secret  
 handler = WebhookHandler('bc9f08c9c29eccb41c7b5b8102b55fd7')
-#users = np.array(('0','0',0)) #userID,level,point
+#users = np.array(('0','0',0)) #userID,level_L,point
 
-##出題小老師  初始抓資料＆資料處理------------------------------------------------
-level = 1 #預設level 1
-isAsked = False
-isChangingLevel = True
-index = 0
+##聽力  初始抓資料＆資料處理------------------------------------------------
+level_L = 1 #預設level 1
+star_num = 0
+isAsked_L = False
+isChangingLevel_L = True
+index_L = 0
 GDriveJSON = 'question.json'
-GSpreadSheet = 'cilab_ChatBot_test'
+GSpreadSheet_L = 'cilab_ChatBot_listening'
 gc = pygsheets.authorize(service_account_file='question.json')
-survey_url = 'https://docs.google.com/spreadsheets/d/1Zf5Qr_dp5GjYZJbxuVKl283fIRKUgs2q9nYNBeTWKJ8/edit#gid=0'
-sh = gc.open_by_url(survey_url)
+survey_url_L = 'https://docs.google.com/spreadsheets/d/1e1hCM0yFzwQkzfdzJGCioLCvnPNJHw9IPHqz4sSEsjg/edit#gid=0'
+sh_L = gc.open_by_url(survey_url_L)
 
 #取得所有工作表名稱
-worksheet_list = sh.worksheets()
-print("worksheet_list",worksheet_list)
-ws1 = worksheet_list[0]
-ws2 = worksheet_list[1]
-ws3 = worksheet_list[2]
+worksheet_list_L = sh_L.worksheets()
+print("worksheet_list_L",worksheet_list_L)
+ws1_L = worksheet_list_L[0]
+ws2_L = worksheet_list_L[1]
+ws3_L = worksheet_list_L[2]
 
-ws1.export(filename='df1') 
-ws2.export(filename='df2') 
-ws3.export(filename='df3') 
-data1 = pd.read_csv('df1.csv') #type: <class 'pandas.core.frame.DataFrame'>
-data2 = pd.read_csv('df2.csv')
-data3 = pd.read_csv('df3.csv')
+ws1_L.export(filename='df1_L') 
+ws2_L.export(filename='df2_L') 
+ws3_L.export(filename='df3_L') 
+data1_L = pd.read_csv('df1_L.csv') #type: <class 'pandas.core.frame.DataFrame'>
+data2_L = pd.read_csv('df2_L.csv')
+data3_L = pd.read_csv('df3_L.csv')
 
-
-def getSheet():  #打亂該sheet順序，並存成dictionary格式  
-    if(level == 3):
-        data = data3
-    elif(level == 2):
-        data = data2
+def getSheet(Qlevel):  #TODO 傳變數level,判斷是聽力還是出題小老師 #打亂該sheet順序，並存成dictionary格式  
+    if(Qlevel == 3):
+        data = data3_L
+    elif(Qlevel == 2):
+        data = data2_L
     else:
-        data = data1
+        data = data1_L
 
     df = data.sample(frac =1,random_state=1) #Random打亂資料再取n筆題   
     #df = np.random.sample(data)
@@ -79,7 +79,7 @@ def getSheet():  #打亂該sheet順序，並存成dictionary格式
     qNum = len(sheet["question"])
     return sheet,qNum
 
-sheet,qNum = getSheet()
+sheet,qNum = getSheet(level_L)
 ##------------------------------------------------
 
 # 監聽所有來自 /callback 的 Post Request
@@ -97,23 +97,24 @@ def callback():
         abort(400)
     return 'OK'
 
-##出題小老師  處理訊息------------------------------------------------
+##聽力測驗  處理訊息------------------------------------------------
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):  
-    global isAsked
-    global index
-    global isChangingLevel
+    global isAsked_L
+    global index_L
+    global isChangingLevel_L
     replytext = event.message.text
     #myId = event.source.user_id
     if event.message.type == 'text':   
-        if (isChangingLevel == True or replytext =='?'):   
-            isChangingLevel = True
-            isAsked = False
+        if (isChangingLevel_L == True or replytext =='?'):   
+            isChangingLevel_L = True
+            isAsked_L = False
             buttons_template = TemplateSendMessage (
                     alt_text = 'Buttons Template',
                     template = ButtonsTemplate (
-                        title = '歡迎來到資策會LineBot英文小老師！',
-                        text = '請點選題目程度~',
+                        title = '聽力練習',
+                        text = '總是聽不懂別人在說什麼嗎?',
+                        thumbnail_image_url='https://upload.cc/i1/2020/06/08/jhziMK.png',
                         actions = [
                                 PostbackTemplateAction(
                                     label = "初級", 
@@ -135,43 +136,36 @@ def handle_message(event):
                 )
             line_bot_api.reply_message(event.reply_token, buttons_template)  
         else:
-            if( isAsked == False ):     
-                print(sheet["question"][index])
-                print("1:", sheet["option1"][index], "\n2:", sheet["option2"][index], "\n3:", sheet["option3"][index],
-                        "\n4:", sheet["option4"][index], "\n")
-
-                option = ("1:" + sheet["option1"][index] + "\n2:" + sheet["option2"][index] + "\n3:" + 
-                            sheet["option3"][index] + "\n4:" + sheet["option4"][index] + "\n")
-                question = sheet["question"][index]
-                ask = question + "\n" + option  
-                isAsked = True
+            if( isAsked_L == False ):     
+                question = sheet["question"][index_L]
+                isAsked_L = True
                 
                 buttons_template = TemplateSendMessage (
                     alt_text = 'Buttons Template',
                     template = ButtonsTemplate (
-                        title = '出題小老師',
-                        text = question,
-                        #thumbnail_image_url = '顯示在開頭的大圖片網址',
+                        title = question,
+                        text = '點我聽題目',
+                        thumbnail_image_url='https://upload.cc/i1/2020/05/27/Hdyx42.jpg',
+                        default_action = {
+                            "type": "uri",
+                            "label": "View detail",
+                            "uri": "https://developers.line.biz/en/reference/messaging-api/#buttons"
+                        },
                         actions = [
                                 PostbackTemplateAction(
-                                    label = ("(1) " + sheet["option1"][index]), 
+                                    label = ("(1) " + sheet["option1"][index_L]), 
                                     text = "(1)",
                                     data = '1'
                                 ),
                                 PostbackTemplateAction(
-                                    label = "(2) " + sheet["option2"][index],
+                                    label = "(2) " + sheet["option2"][index_L],
                                     text = "(2)",
                                     data = '2'
                                 ),
                                 PostbackTemplateAction(
-                                    label = "(3) " + sheet["option3"][index],
+                                    label = "(3) " + sheet["option3"][index_L],
                                     text = "(3)",
                                     data = '3'
-                                ),
-                                PostbackTemplateAction(
-                                    label = "(4) " + sheet["option4"][index],
-                                    text = "(4)",
-                                    data = '4'
                                 )
                         ]
                     )
@@ -181,71 +175,84 @@ def handle_message(event):
     #print(event.reply_token)
     #print("=========================")
 
-#出題小老師  回饋判斷------------------------------------------------
+#聽力測驗  回饋判斷------------------------------------------------
 @handler.add(PostbackEvent)
 def handle_postback(event):
     print("---Feedback---")
-    global isAsked
-    global index
+    global isAsked_L
+    global index_L
     global sheet
     global qNum
+    global star_num
 
-    if(isChangingLevel==True):
+    if(isChangingLevel_L==True):
         levelinput = event.postback.data
         myResult = setLevel(levelinput) 
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = myResult))
     else:    
-        print("correct answer = ",str(sheet["answer"][index]))
-        print("index = ", index)
+        print("correct answer = ",str(sheet["answer"][index_L]))
+        print("index_L = ", index_L)
         answer = event.postback.data
-        if answer != str(sheet["answer"][index]):
-            feedback = sheet["feedback"][index]
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text = feedback))
-            isAsked = False       
+        if answer != str(sheet["answer"][index_L]):
+            if(index_L >= qNum - 1): #做完本輪題庫數目
+                print('恭喜你做完這次的聽力練習了!')
+                end_feedbck =("恭喜你做完這次的聽力練習了!\n你獲得的星星是"+ str(star_num) +"顆哦!!你好棒!")
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = end_feedbck))
+            else:
+                feedback = sheet["feedback"][index_L]
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = feedback))
+            isAsked_L = False       
         else:
-            print('答對了！你真棒！')
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text = '答對了！你真棒！'))
-            isAsked = False
+            star_num += 1
+            if(index_L >= qNum - 1):#做完本輪題庫數目
+                print('恭喜你做完這次的聽力練習了!')
+                end_feedbck =("恭喜你做完這次的聽力練習了!\n你獲得的星星是"+ str(star_num) +"顆哦!!你好棒!")
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = end_feedbck))
+            else:
+                print('恭喜你答對了!給你一個小星星!')
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = '恭喜你答對了!給你一個小星星!\n'))
 
-        if index < qNum - 1:
-            index += 1
-        else:
-            index = 0
-            sheet,qNum = getSheet()
+            isAsked_L = False
+
+        if index_L < qNum - 1:
+            index_L += 1
+        else:#做完本輪題庫數目
+            index_L = 0
+            star_num = 0
+            sheet,qNum = getSheet(level_L)
             print("new sheet",sheet)
             print("new qNum",qNum)
-        print("index after = ", index)
+        print("index_L after = ", index_L)
 
-##出題小老師  設定Level------------------------------------------------
+##聽力測驗  設定Level------------------------------------------------
 def setLevel(levelinput):
     print("---Changing Level---")
-    global data
     global sheet
     global qNum
-    global level
-    global isChangingLevel
+    global level_L
+    global isChangingLevel_L
    
     if (levelinput=='L'):
-        level = 1
-        isChangingLevel = False
+        level_L = 1
+        isChangingLevel_L = False
         myResult= ("目前程度切換至初級 \n 請任意輸入 將開始出題～～")
         
     elif (levelinput=='M'):
-        level = 2
-        isChangingLevel = False
+        level_L = 2
+        isChangingLevel_L = False
         myResult= ("目前程度切換至中級\n 請任意輸入 將開始出題～～")    
     elif (levelinput=='H'):
-        level = 3
-        isChangingLevel = False
+        level_L = 3
+        isChangingLevel_L = False
         myResult= ("目前程度切換至高級\n 請任意輸入 將開始出題～～")  
     else:       
-        isChangingLevel = True
+        isChangingLevel_L = True
         myResult = "N"
     
-    if isChangingLevel == False:
-        sheet,qNum = getSheet()
-        print("level get sheet",sheet)
-        print("level get qNum",qNum)
+    if isChangingLevel_L == False:
+        sheet,qNum = getSheet(level_L)
+        print("level_L get sheet",sheet)
+        print("level_L get qNum",qNum)
       
     return myResult
 
@@ -277,31 +284,8 @@ def levelButton(event):
                 )
     line_bot_api.reply_message(event.reply_token, buttons_template)  
 
-##出題小老師  End------------------------------------------------
+##  End------------------------------------------------
 
-
-
-def translate(event):
-    translator = Translator()
-    lang = translator.detect(event.message.text)
-    print("Lang=",lang.lang)
-    if event.message.type == 'text':
-        if lang.lang == "zh-CN" :
-            print("this is Chinese")
-            translateMessage = translator.translate(event.message.text, dest='en')
-            print(translateMessage.text)
-            message = TextSendMessage(text=translateMessage.text)
-        elif lang.lang =="en":
-            print("this is English")
-            translateMessage = translator.translate(event.message.text, dest='zh-tw')
-            print(translateMessage.text)
-            message = TextSendMessage(text=translateMessage.text)
-        else:
-            print("I can't translate this kind of message")
-            message = TextSendMessage(text="抱歉！機器人無法翻譯這種語言喔～")
-    else:
-        message = TextSendMessage(text="抱歉！機器人無法翻譯這種訊息呢～")
-    print("message=",message)    
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
