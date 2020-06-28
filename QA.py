1474d41fec564c628b7ffe07183317da0184c153
Irene <lai860423@gmail.com>
2020-06-28 12:29:44 +0800

    sheet tail

--- QA.py
+++ QA.py
@@ -12,11 +12,11 @@ from googletrans import Translator
 
 def QA_Img(sheet,subindex):
     question = sheet["question"][subindex]
-    
     print("option1 = ",sheet["option1"][subindex])
     print("option2 = ",sheet["option2"][subindex])
     print("option3 = ",sheet["option3"][subindex])
     print("Question = ",question)  #question 是 url 網址
+
     QA_img = CarouselContainer (
                         contents = [
                             BubbleContainer (
@@ -127,6 +127,12 @@ def QA_Img(sheet,subindex):
     return QA_img   
 
 def QA_Tail(sheet,subindex):
+    question = sheet["question"][subindex]
+    print("option1 = ",sheet["option1"][subindex])
+    print("option2 = ",sheet["option2"][subindex])
+    print("option3 = ",sheet["option3"][subindex])
+    print("Question = ",question)  #question 是 url 網址
+
     QA_tail = BubbleContainer (
         direction='ltr',
         header = BoxComponent(
@@ -139,7 +145,7 @@ def QA_Tail(sheet,subindex):
             layout='vertical',
             contents=[
                 ButtonComponent(
-                    action = URIAction(label = '聽題目', uri = 'https://linecorp.com'),
+                    action = URIAction(label = '聽題目', uri = question),
                     color = '#3B9A9C',
                     margin = 'lg',
                     style = 'primary',
@@ -148,19 +154,19 @@ def QA_Tail(sheet,subindex):
                 TextComponent(text='選出所聽到的單字的字尾音', size='md', align = 'center'),
                 SeparatorComponent(margin='xl',color='#A89F9F'),
                 ButtonComponent(
-                    action = PostbackAction(label = '(1) f', data = '1', text = '(1) f'),
+                    action = PostbackAction("(1) " +label = sheet["option1"][subindex], data = '1', text = "(1) " +label = sheet["option1"][subindex]),
                     color = '#46549B',
                     margin = 'md',
                     style = 'primary'
                 ),
                     ButtonComponent(
-                    action = PostbackAction(label = '(2) b', data = '2', text = '(2) b'),
+                    action = PostbackAction(label = "(2) " +label = sheet["option2"][subindex], data = '2', text = "(2) " +label = sheet["option2"][subindex]),
                     color = '#7E318E',
                     margin = 'md',
                     style = 'primary'
                 ),
                     ButtonComponent(
-                    action = PostbackAction(label = '(3) m', data = '3', text = '(3) m'),
+                    action = PostbackAction(label = "(3) " +label = sheet["option3"][subindex], data = '3', text = "(3) " +label = sheet["option3"][subindex]),
                     color = '#CD2774',
                     margin = 'md',
                     style = 'primary',
@@ -172,6 +178,12 @@ def QA_Tail(sheet,subindex):
     return QA_tail
 
 def QA_Word(sheet,subindex):
+    question = sheet["question"][subindex]
+    print("option1 = ",sheet["option1"][subindex])
+    print("option2 = ",sheet["option2"][subindex])
+    print("option3 = ",sheet["option3"][subindex])
+    print("Question = ",question)  #question 是 url 網址
+
     QA_word = BubbleContainer (
         direction='ltr',
         header = BoxComponent(
@@ -184,7 +196,7 @@ def QA_Word(sheet,subindex):
             layout='vertical',
             contents=[
                 ButtonComponent(
-                    action = URIAction(label = '聽題目', uri = 'https://linecorp.com'),
+                    action = URIAction(label = '聽題目', uri = question),
                     color = '#3B9A9C',
                     margin = 'lg',
                     style = 'primary',
