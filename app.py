c8fb1707f2a31cf07659ed5fc8d992a186d36fe4
Irene <lai860423@gmail.com>
2020-06-29 11:16:26 +0800

    出題小老師

--- app.py
+++ app.py
@@ -9,12 +9,11 @@ from linebot.models import *
 import numpy as np
 import pandas as pd
 from googletrans import Translator
-import QA
-##聽力測驗  import-----------------------------------------------
+
+##出題小老師  import-----------------------------------------------
 import sys
 import datetime
 import pygsheets
-
 
 app = Flask(__name__)
 
@@ -22,81 +21,67 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('mIg76U+23oiAkDahsjUoK7ElbuYXzLDJcGXaEjaJIfZ+mMqOO3BvX+RlQIzx/Zu0Smy8W08i01F38xGDg6r/thlWLwGxRvcgExAucwMag8KPVAkBFfSLUvgcrxQS4HBzOGIBxoo+zRSJhOFoBEtCVQdB04t89/1O/w1cDnyilFU=')
 #Channel Secret  
 handler = WebhookHandler('bc9f08c9c29eccb41c7b5b8102b55fd7')
-#users = np.array(('0','0',0)) #userID,level_L,point
+#users = np.array(('0','0',0)) #userID,level,point
 
-##聽力  變數------------------------------------------------
-level_L = 1 # 預設level 1
-type_L = 1 # 3種題目類型
-qNum = 20 # 每輪題目數量
-star_num = 0 #集點
-isAsked_L = False #出題與否
-isChangingLevel_L = True
-index_L = 0 #第幾題
-subindex = 0
-##-----------------------------------------------------------------------------------
-##聽力  初始抓資料＆資料處理
+##出題小老師  變數------------------------------------------------
+
+level = 1 #預設level 1
+isAsked = False
+isChangingLevel = True
+index = 0
+# 初始抓資料＆資料處理------------------------------------------------
 GDriveJSON = 'question.json'
-GSpreadSheet_L = 'cilab_ChatBot_listening'
-gc = pygsheets.authorize(service_account_file='question.json') #檔案裡的google sheet js檔
-survey_url_L = 'https://docs.google.com/spreadsheets/d/1e1hCM0yFzwQkzfdzJGCioLCvnPNJHw9IPHqz4sSEsjg/edit#gid=0'
-sh_L = gc.open_by_url(survey_url_L)
-sh_L.worksheet_by_title('L1_img').export(filename='L1_img')
-sh_L.worksheet_by_title('L1_tail').export(filename='L1_tail')
-sh_L.worksheet_by_title('L1_word').export(filename='L1_word')
-sh_L.worksheet_by_title('L1_sen').export(filename='L1_sen')
-sh_L.worksheet_by_title('L2_img').export(filename='L2_img')
-sh_L.worksheet_by_title('L2_tail').export(filename='L2_tail')
-sh_L.worksheet_by_title('L2_word').export(filename='L2_word')
-sh_L.worksheet_by_title('L2_sen').export(filename='L2_sen')
-sh_L.worksheet_by_title('L3_img').export(filename='L3_img')
-sh_L.worksheet_by_title('L3_tail').export(filename='L3_tail')
-sh_L.worksheet_by_title('L3_word').export(filename='L3_word')
-sh_L.worksheet_by_title('L3_sen').export(filename='L3_sen')
-#worksheet_list_L[11].export(filename='L3_sen')
+GSpreadSheet = 'cilab_ChatBot_test'
+gc = pygsheets.authorize(service_account_file='question.json')
+survey_url = 'https://docs.google.com/spreadsheets/d/1Zf5Qr_dp5GjYZJbxuVKl283fIRKUgs2q9nYNBeTWKJ8/edit#gid=0'
+sh = gc.open_by_url(survey_url)
+sh.worksheet_by_title('L1_Word').export(filename='L1_Word')
+sh.worksheet_by_title('L1_Grammar').export(filename='L1_Grammar')
+sh.worksheet_by_title('L1_Cloze').export(filename='L1_Cloze')
+sh.worksheet_by_title('L2_Word').export(filename='L2_Word')
+sh.worksheet_by_title('L2_Grammar').export(filename='L2_Grammar')
+sh.worksheet_by_title('L2_Cloze').export(filename='L2_Cloze')
+sh.worksheet_by_title('L3_Word').export(filename='L3_Word')
+sh.worksheet_by_title('L3_Grammar').export(filename='L3_Grammar')
+sh.worksheet_by_title('L3_Cloze').export(filename='L3_Cloze')
 
-L1_img = pd.read_csv('L1_img.csv') #type: <class 'pandas.core.frame.DataFrame'>
-L1_tail = pd.read_csv('L1_tail.csv')
-L1_word = pd.read_csv('L1_word.csv')
-L1_sen = pd.read_csv('L1_sen.csv')
-L2_img = pd.read_csv('L2_img.csv') 
-L2_tail = pd.read_csv('L2_tail.csv') 
-L2_word = pd.read_csv('L2_word.csv')
-L2_sen = pd.read_csv('L2_sen.csv')
-L3_img = pd.read_csv('L3_img.csv') 
-L3_tail = pd.read_csv('L3_tail.csv') 
-L3_word = pd.read_csv('L3_word.csv')
-L3_sen = pd.read_csv('L3_sen.csv')
-##-----------------------------------------------------------------------------------
-#四種問題類型
-def getSheet(Qlevel):   
-    if(Qlevel == 3):
-        sheet_img = L3_img
-        sheet_tail = L3_tail
-        sheet_word = L3_word
-        sheet_sen = L3_sen  
+L1_Word = pd.read_csv('L1_Word.csv') #type: <class 'pandas.core.frame.DataFrame'>
+L1_Grammar = pd.read_csv('L1_Grammar.csv')
+L1_Cloze = pd.read_csv('L1_Cloze.csv')
+L2_Word = pd.read_csv('L2_Word.csv') 
+L2_Grammar = pd.read_csv('L2_Grammar.csv') 
+L2_Cloze = pd.read_csv('L2_Cloze.csv')
+L3_Word = pd.read_csv('L3_Word.csv') 
+L3_Grammar = pd.read_csv('L3_Grammar.csv') 
+L3_Cloze = pd.read_csv('L3_Cloze.csv')
 
-    elif(Qlevel == 2):
-        sheet_img = L2_img
-        sheet_tail = L2_tail
-        sheet_word = L2_word
-        sheet_sen = L2_sen 
+
+
+def getSheet(level):  #打亂該sheet順序，並存成dictionary格式  
+    if(level == 3):
+        sheet_Word = L3_Word
+        sheet_Grammar = L3_Grammar
+        sheet_Cloze = L3_Cloze
+    elif(level == 2):
+        sheet_Word = L3_Word
+        sheet_Grammar = L3_Grammar
+        sheet_Cloze = L3_Cloze
     else:
-        sheet_img = L1_img
-        sheet_tail = L1_tail
-        sheet_word = L1_word
-        sheet_sen = L1_sen 
-
-    return sheet_img, sheet_tail, sheet_word, sheet_sen
+        sheet_Word = L3_Word
+        sheet_Grammar = L3_Grammar
+        sheet_Cloze = L3_Cloze
+    return sheet_Word, sheet_Grammar, sheet_Cloze
 
 def editSheet(data):
-    pre_sheet = data.sample(frac =1,random_state=1) #Random打亂資料再取n筆題 
-    question = pre_sheet.iloc[:,0]
-    option1 = pre_sheet.iloc[:,1]
-    option2 = pre_sheet.iloc[:,2]
-    option3 = pre_sheet.iloc[:,3]
-    option4 = pre_sheet.iloc[:,4]
-    feedback = pre_sheet.iloc[:,5]
-    answer = pre_sheet.iloc[:,6]
+    df = data.sample(frac =1,random_state=1) #Random打亂資料再取n筆題   
+    print("getSheet df = ",df)
+    question = df.iloc[:,0]
+    option1 = df.iloc[:,1]
+    option2 = df.iloc[:,2]
+    option3 = df.iloc[:,3]
+    option4 = df.iloc[:,4]
+    feedback = df.iloc[:,5]
+    answer = df.iloc[:,6]
     sheet = {
         "question": question,
         "option1": option1,
@@ -106,12 +91,13 @@ def editSheet(data):
         "feedback": feedback,
         "answer": answer
     }
-    #qNum = len(sheet["question"])
-    return sheet
+    qNum = len(sheet["question"])
+    return sheet,qNum
 
-data_img, data_tail, data_word, data_sen = getSheet(level_L)
-sheet = editSheet(data_img) 
-##-----------------------------------------------------------------------------------
+data_Word, data_Grammar, data_Cloze = getSheet(level)
+sheet, qNum = editSheet(data_Word) 
+##------------------------------------------------
+
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -126,26 +112,25 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-##-----------------------------------------------------------------------------------
-#處理訊息
+
+##出題小老師  處理訊息------------------------------------------------
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):  
-    global isAsked_L
-    global index_L
-    global isChangingLevel_L
-    global sheet,subindex
+    global isAsked
+    global index
+    global isChangingLevel
+    global sheet
     replytext = event.message.text
     #myId = event.source.user_id
     if event.message.type == 'text':   
-        if (isChangingLevel_L == True or replytext =='?'):   
-            isChangingLevel_L = True
-            isAsked_L = False
+        if (isChangingLevel == True or replytext =='?'):   
+            isChangingLevel = True
+            isAsked = False
             buttons_template = TemplateSendMessage (
                     alt_text = 'Buttons Template',
                     template = ButtonsTemplate (
-                        title = '聽力練習',
-                        text = '總是聽不懂別人在說什麼嗎?',
-                        thumbnail_image_url='https://upload.cc/i1/2020/06/08/jhziMK.png',
+                        title = '歡迎來到資策會LineBot英文小老師！',
+                        text = '請點選題目程度~',
                         actions = [
                                 PostbackTemplateAction(
                                     label = "初級", 
@@ -167,109 +152,116 @@ def handle_message(event):
                 )
             line_bot_api.reply_message(event.reply_token, buttons_template)  
         else:
-            if( isAsked_L == False ):   
-                print("選完階級！")
-                isAsked_L = True
-                print("index_L",index_L)
-                subindex = index_L%5
-                print("subindex = ",subindex)
-                if index_L < 5:
-                    sheet = editSheet(data_img)
-                    QA_bubble = QA.QA_Img(sheet,index_L,subindex)
-                elif index_L < 10:
-                    sheet = editSheet(data_tail)
-                    QA_bubble = QA.QA_Tail(sheet,index_L,subindex)
-                elif index_L < 15:
-                    sheet = editSheet(data_word)
-                    QA_bubble = QA.QA_Word(sheet,index_L,subindex)
-                else:
-                    sheet = editSheet(data_sen) 
-                    QA_bubble = QA.QA_Sentence(sheet,index_L,subindex)    
-            
-                message = FlexSendMessage(alt_text="QA_bubble", contents = QA_bubble)
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    message
+            if( isAsked == False ):
+                #sheet, qNum = editSheet(data_Word)    
+                print(sheet["question"][index])
+                print("1:", sheet["option1"][index], "\n2:", sheet["option2"][index], "\n3:", sheet["option3"][index],
+                        "\n4:", sheet["option4"][index], "\n")
+
+                #option = ("1:" + sheet["option1"][index] + "\n2:" + sheet["option2"][index] + "\n3:" + 
+                            #sheet["option3"][index] + "\n4:" + sheet["option4"][index] + "\n")
+                question = sheet["question"][index]
+                #ask = question + "\n" + option  
+                isAsked = True
+                
+                buttons_template = TemplateSendMessage (
+                    alt_text = 'Buttons Template',
+                    template = ButtonsTemplate (
+                        title = '出題小老師',
+                        text = question,
+                        actions = [
+                                PostbackTemplateAction(
+                                    label = ("(1) " + sheet["option1"][index]), 
+                                    text = "(1)",
+                                    data = '1'
+                                ),
+                                PostbackTemplateAction(
+                                    label = "(2) " + sheet["option2"][index],
+                                    text = "(2)",
+                                    data = '2'
+                                ),
+                                PostbackTemplateAction(
+                                    label = "(3) " + sheet["option3"][index],
+                                    text = "(3)",
+                                    data = '3'
+                                ),
+                                PostbackTemplateAction(
+                                    label = "(4) " + sheet["option4"][index],
+                                    text = "(4)",
+                                    data = '4'
+                                )
+                        ]
+                    )
                 )
-##-----------------------------------------------------------------------------------
-#回饋判斷
+                line_bot_api.reply_message(event.reply_token, buttons_template)   
+    #print("=======Reply Token=======")
+    #print(event.reply_token)
+    #print("=========================")
+
+#出題小老師  回饋判斷------------------------------------------------
 @handler.add(PostbackEvent)
 def handle_postback(event):
     print("---Feedback---")
-    global isAsked_L
-    global index_L
-    global sheet,subindex
+    global isAsked
+    global index
+    global sheet
     global qNum
-    global star_num
-    global data_img, data_tail, data_word, data_sen
 
-    if(isChangingLevel_L==True):
+    if(isChangingLevel==True):
         levelinput = event.postback.data
         myResult = setLevel(levelinput) 
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = myResult))
     else:    
-        print("correct answer = ",str(sheet["answer"][subindex]))
-        print("answer index_L = ", index_L)
-        print("answer subindex = ", subindex)
+        print("correct answer = ",str(sheet["answer"][index]))
+        print("index = ", index)
         answer = event.postback.data
-        if answer != str(sheet["answer"][subindex]):
-            if(index_L >= qNum - 1): #做完本輪題庫數目
-                print('恭喜你做完這次的聽力練習了!')
-                end_feedbck =("恭喜你做完這次的聽力練習了!\n你獲得的星星是"+ str(star_num) +"顆哦!!你好棒!")
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = end_feedbck))
-            else:
-                feedback = sheet["feedback"][subindex]
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = feedback))
-                isAsked_L = False       
+        if answer != str(sheet["answer"][index]):
+            feedback = sheet["feedback"][index]
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text = feedback))
+            isAsked = False       
         else:
-            star_num += 1
-            if(index_L >= qNum - 1):#做完本輪題庫數目
-                print('恭喜你做完這次的聽力練習了!')
-                end_feedbck =("恭喜你做完這次的聽力練習了!\n你獲得的星星是"+ str(star_num) +"顆哦!!你好棒!")
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = end_feedbck))
-            else:
-                print('恭喜你答對了!給你一個小星星!')
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text = '恭喜你答對了!給你一個小星星!\n'))
-                isAsked_L = False
+            print('答對了！你真棒！')
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text = '答對了！你真棒！'))
+            isAsked = False
 
-        if index_L < qNum - 1:
-            index_L += 1
-        else:#做完本輪題庫數目
-            index_L = 0
-            star_num = 0
-            #data_img, data_tail, data_word, data_sen = getSheet(level_L)
-            #sheet = editSheet(data_img) 
+        if index < qNum - 1:
+            index += 1
+        else:
+            index = 0
+            #data_Word, data_Grammar, data_Cloze = getSheet(level)
+            #sheet, qNum = editSheet(data_Word) 
             #print("new sheet",sheet)
-        print("index_L after = ", index_L)
-##-----------------------------------------------------------------------------------
-#設定Level------------------------------------------------
+            #print("new qNum",qNum)
+
+        print("index after = ", index)
+
+##出題小老師  設定Level------------------------------------------------
 def setLevel(levelinput):
     print("---Changing Level---")
-    global data_img, data_tail, data_word, data_sen
-    global level_L
-    global isChangingLevel_L
-    #global pre_sheet
+    global sheet, data_Word, data_Grammar, data_Cloze
+    global qNum
+    global level
+    global isChangingLevel
+   
     if (levelinput=='L'):
-        level_L = 1
-        isChangingLevel_L = False
-        myResult= ("目前程度切換至初級 \n 以下將開始出題")
+        level = 1
+        isChangingLevel = False
+        myResult= ("目前程度切換至初級 \n 請任意輸入 將開始出題～～")
         
     elif (levelinput=='M'):
-        level_L = 2
-        isChangingLevel_L = False
-        myResult= ("目前程度切換至中級\n 以下將開始出題")    
+        level = 2
+        isChangingLevel = False
+        myResult= ("目前程度切換至中級\n 請任意輸入 將開始出題～～")    
     elif (levelinput=='H'):
-        level_L = 3
-        isChangingLevel_L = False
-        myResult= ("目前程度切換至高級\n 以下將開始出題")  
+        level = 3
+        isChangingLevel = False
+        myResult= ("目前程度切換至高級\n 請任意輸入 將開始出題～～")  
     else:       
-        isChangingLevel_L = True
+        isChangingLevel = True
         myResult = "N"
     
-    if isChangingLevel_L == False:
-        data_img, data_tail, data_word, data_sen = getSheet(level_L)
-        #sheet = editSheet(pre_sheet)
-        print("level_L get sheet",sheet)
+    if isChangingLevel == False:
+        data_Word, data_Grammar, data_Cloze = getSheet(level)
       
     return myResult
 
@@ -278,6 +270,8 @@ def levelButton(event):
                     alt_text = 'Buttons Template',
                     template = ButtonsTemplate (
                         title = '請選擇出題小老師題目程度～',
+                        #text = question,
+                        #thumbnail_image_url = '顯示在開頭的大圖片網址',
                         actions = [
                                 PostbackTemplateAction(
                                     label = "初級", 
@@ -299,51 +293,32 @@ def levelButton(event):
                 )
     line_bot_api.reply_message(event.reply_token, buttons_template)  
 
-##  End------------------------------------------------
+##出題小老師  End------------------------------------------------
 
+
+
+def translate(event):
+    translator = Translator()
+    lang = translator.detect(event.message.text)
+    print("Lang=",lang.lang)
+    if event.message.type == 'text':
+        if lang.lang == "zh-CN" :
+            print("this is Chinese")
+            translateMessage = translator.translate(event.message.text, dest='en')
+            print(translateMessage.text)
+            message = TextSendMessage(text=translateMessage.text)
+        elif lang.lang =="en":
+            print("this is English")
+            translateMessage = translator.translate(event.message.text, dest='zh-tw')
+            print(translateMessage.text)
+            message = TextSendMessage(text=translateMessage.text)
+        else:
+            print("I can't translate this kind of message")
+            message = TextSendMessage(text="抱歉！機器人無法翻譯這種語言喔～")
+    else:
+        message = TextSendMessage(text="抱歉！機器人無法翻譯這種訊息呢～")
+    print("message=",message)    
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-
-# QAsort_bubble = BubbleContainer (
-#                 header = BoxComponent(
-#                     layout='vertical',
-#                     contents=[
-#                         TextComponent(text='請選擇題目類型', weight='bold', size='xl', color = '#000000')                   
-#                     ]
-#                 ),
-#                 body = BoxComponent(
-#                     layout='vertical',
-#                     contents=[
-#                         ButtonComponent(
-#                             action = PostbackAction(label = '詞彙練習', data = 'L', text = '詞彙練習'),
-#                             color = '#001774',
-#                             style = 'primary',
-#                             gravity = 'center'
-#                         ),
-#                         ButtonComponent(
-#                             action = PostbackAction(label = '文法練習', data = 'M', text = '文法練習'),
-#                             color = '#FF595D',
-#                             margin = 'md',           
-#                             style = 'primary',
-#                             gravity = 'center'
-#                         ),
-#                         ButtonComponent(
-#                             action = PostbackAction(label = '克漏字練習', data = 'H', text = '克漏字練習'),
-#                             color = '#FFB54A',
-#                             margin = 'md',           
-#                             style = 'primary',
-#                             gravity = 'center'
-#                         )
-#                     ]
-#                 )
-#             )   
-            
-#             #line_bot_api.reply_message(event.reply_token, buttons_template)  
-#             message = FlexSendMessage(alt_text="QAsort_bubble", contents = QAsort_bubble)
-#             line_bot_api.reply_message(
-#                 event.reply_token,
-#                 message
-#             )
