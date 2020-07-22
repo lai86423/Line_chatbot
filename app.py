ff96f23f2f18671b427fe14a46240706ce2bd8a3
Irene <lai860423@gmail.com>
2020-07-22 15:22:31 +0800

    re questtion

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def handle_message(event):
             message = TextSendMessage(text="歡迎來到聽力練習！\n\n在這邊可以選擇適合你的難易度。\n\n題目分為發音、詞彙以及句子，答題越精確獲得的星星數越多哦！\n\n第一次就答對：🌟🌟\n第二次才答對：🌟\n第三次才答對：❌")
             line_bot_api.push_message(myId, message)
             isInit_L=False
-        elif(isChangingLevel_L == True):   
+        if(isChangingLevel_L == True):   
             isAsked_L = False
             setlevel_bubble = levelBubble()
             line_bot_api.reply_message(event.reply_token, setlevel_bubble)  
@@ -152,8 +152,8 @@ def handle_message(event):
             if( isAsked_L == False ): 
                 isAsked_L = True
                 QA_bubble = Question()
-            message = FlexSendMessage(alt_text="QA_bubble", contents = QA_bubble)
-            line_bot_api.reply_message(event.reply_token, message)
+                message = FlexSendMessage(alt_text="QA_bubble", contents = QA_bubble)
+                line_bot_api.reply_message(event.reply_token, message)
 ##-----------------------------------------------------------------------------------
 #回饋判斷
 @handler.add(PostbackEvent)
