407bb6fa20dbd220146b55a50832be0df09a2a9f
Irene <lai860423@gmail.com>
2020-08-04 15:01:02 +0800

    article sned bool ==7

--- app.py
+++ app.py
@@ -168,13 +168,15 @@ def handle_postback(event):
         line_bot_api.reply_message(event.reply_token,message) 
 
     elif(event.postback.data == "start"):  
-        print("******subindex_Q index_Q%8 count_Q",index_Q,index_Q%8,count_Q)
-        if(index_Q >= 7 and index_Q%8 == 0 and count_Q == 1):
+        print("******index_Q index_Q%8 count_Q",index_Q,index_Q%8,count_Q)
+        if(index_Q == 7 and index_Q%8 == 0 and count_Q == 1):
             print("article!?")
             sheet_article = editSheet(data_Reading) 
             QA_bubble_article = QA_Bubble.Article(sheet_article,subindex_Q)
             article = FlexSendMessage(alt_text="QA_bubble", contents = QA_bubble_article)
             line_bot_api.push_message(myId, article)
+            #isStart_Q = True
+        
         isStart_Q = True
 
     elif(isStart_Q == True): 
