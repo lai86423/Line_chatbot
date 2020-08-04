593458178e3a69595ddc1cf4f3b0aaf90d47ecd9
Irene <lai860423@gmail.com>
2020-08-04 14:53:17 +0800

    article send time move to postback id

--- app.py
+++ app.py
@@ -168,12 +168,13 @@ def handle_postback(event):
         line_bot_api.reply_message(event.reply_token,message) 
 
     elif(event.postback.data == "start"):  
-        print("article")
-        #if(subindex_Q >= 7 and index_Q%8 == 0 and count_Q == 0):
-        sheet_article = editSheet(data_Reading) 
-        QA_bubble_article = QA_Bubble.Article(sheet_article,subindex_Q)
-        article = FlexSendMessage(alt_text="QA_bubble", contents = QA_bubble_article)
-        line_bot_api.push_message(myId, article)
+        print("******subindex_Q index_Q%8 count_Q",subindex_Q,index_Q%8,count_Q)
+        if(subindex_Q >= 7 and index_Q%8 == 0 and count_Q == 0):
+            print("article!?")
+            sheet_article = editSheet(data_Reading) 
+            QA_bubble_article = QA_Bubble.Article(sheet_article,subindex_Q)
+            article = FlexSendMessage(alt_text="QA_bubble", contents = QA_bubble_article)
+            line_bot_api.push_message(myId, article)
         isStart_Q = True
 
     elif(isStart_Q == True): 
