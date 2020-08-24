00767e1c6624c7dfc66c1f629a4d5dade5ce0412
Irene <lai860423@gmail.com>
2020-08-24 08:51:55 +0800

    資策會 表單

--- getVoc.py
+++ getVoc.py
@@ -3,11 +3,11 @@ import pandas as pd
 import pygsheets
 import random
 
-GDriveJSON = 'question.json'
+GDriveJSON = 'formal.json'
 GSpreadSheet_Q = 'Chatbot Voc1200'
-gc_Q = pygsheets.authorize(service_account_file='question.json')
+gc_Q = pygsheets.authorize(service_account_file='formal.json')
 survey_url_P = 'https://docs.google.com/spreadsheets/d/1G5gy7173hk3kcp8AFGM8EOUvb7Wa-KmU4V0dHJQQYEk/edit#gid=831684372'
-sh_P = gc_Q.open_by_url(survey_url_P)
+sh_P = gc_Q.open(GSpreadSheet_Q)
 VocQA = []
 
 def getSheet(level):
