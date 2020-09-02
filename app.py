29bcb6c06334f55b9fb2cb2c5e975ea5b1ad85c5
Irene <lai860423@gmail.com>
2020-09-02 16:37:08 +0800

    intergration test

--- app.py
+++ app.py
@@ -32,9 +32,16 @@ import string
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('Ay6xk+FmKxu4tFPtdzXBMR/V8Mf1GnwNi07Vt9QgOHCHwUCd3x8pdRMu7rTHR1/QWlcVcaaHRzfi9gARYXgNqm7WT7M7YoeWJv+NFkl+iZg5K0jAERYZud6HpNmpVXm6TEIf7ZY1DxnH55E77umPawdB04t89/1O/w1cDnyilFU=')
+# line_bot_api = LineBotApi('Ay6xk+FmKxu4tFPtdzXBMR/V8Mf1GnwNi07Vt9QgOHCHwUCd3x8pdRMu7rTHR1/QWlcVcaaHRzfi9gARYXgNqm7WT7M7YoeWJv+NFkl+iZg5K0jAERYZud6HpNmpVXm6TEIf7ZY1DxnH55E77umPawdB04t89/1O/w1cDnyilFU=')
 
-handler = WebhookHandler('533dbc0dab0d92eea7a87b05cb7e49a6')
+# handler = WebhookHandler('533dbc0dab0d92eea7a87b05cb7e49a6')
+
+#Channel Access Token
+line_bot_api = LineBotApi('mIg76U+23oiAkDahsjUoK7ElbuYXzLDJcGXaEjaJIfZ+mMqOO3BvX+RlQIzx/Zu0Smy8W08i01F38xGDg6r/thlWLwGxRvcgExAucwMag8KPVAkBFfSLUvgcrxQS4HBzOGIBxoo+zRSJhOFoBEtCVQdB04t89/1O/w1cDnyilFU=')
+#Channel Secret  
+handler = WebhookHandler('bc9f08c9c29eccb41c7b5b8102b55fd7')
+#users = np.array(('0','0',0)) #userID,level_Q,point
+
 #-------功能變數--------
 trans = False
 quiz = False
