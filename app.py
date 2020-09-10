f0a45d47e5397b0d8d5dde97c47030f09b5ca008
Irene <lai860423@gmail.com>
2020-09-10 08:49:12 +0800

    lis & QA 第一次測試 都改完 傳書的linebot

--- app.py
+++ app.py
@@ -34,14 +34,14 @@ import string
 app = Flask(__name__)
 
 #書
-# line_bot_api = LineBotApi('Ay6xk+FmKxu4tFPtdzXBMR/V8Mf1GnwNi07Vt9QgOHCHwUCd3x8pdRMu7rTHR1/QWlcVcaaHRzfi9gARYXgNqm7WT7M7YoeWJv+NFkl+iZg5K0jAERYZud6HpNmpVXm6TEIf7ZY1DxnH55E77umPawdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('Ay6xk+FmKxu4tFPtdzXBMR/V8Mf1GnwNi07Vt9QgOHCHwUCd3x8pdRMu7rTHR1/QWlcVcaaHRzfi9gARYXgNqm7WT7M7YoeWJv+NFkl+iZg5K0jAERYZud6HpNmpVXm6TEIf7ZY1DxnH55E77umPawdB04t89/1O/w1cDnyilFU=')
 
-# handler = WebhookHandler('533dbc0dab0d92eea7a87b05cb7e49a6')
+handler = WebhookHandler('533dbc0dab0d92eea7a87b05cb7e49a6')
 
-#映
-line_bot_api = LineBotApi('mIg76U+23oiAkDahsjUoK7ElbuYXzLDJcGXaEjaJIfZ+mMqOO3BvX+RlQIzx/Zu0Smy8W08i01F38xGDg6r/thlWLwGxRvcgExAucwMag8KPVAkBFfSLUvgcrxQS4HBzOGIBxoo+zRSJhOFoBEtCVQdB04t89/1O/w1cDnyilFU=')
+# #映
+# line_bot_api = LineBotApi('mIg76U+23oiAkDahsjUoK7ElbuYXzLDJcGXaEjaJIfZ+mMqOO3BvX+RlQIzx/Zu0Smy8W08i01F38xGDg6r/thlWLwGxRvcgExAucwMag8KPVAkBFfSLUvgcrxQS4HBzOGIBxoo+zRSJhOFoBEtCVQdB04t89/1O/w1cDnyilFU=')
 
-handler = WebhookHandler('bc9f08c9c29eccb41c7b5b8102b55fd7')
+# handler = WebhookHandler('bc9f08c9c29eccb41c7b5b8102b55fd7')
 #-------功能變數--------
 trans = False
 quiz = False
