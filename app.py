9996c8f5682ed385d99becc780bdf7562b15c6e9
Irene <lai860423@gmail.com>
2020-10-09 23:50:26 +0800

    puzzle speech test

--- app.py
+++ app.py
@@ -644,7 +644,7 @@ def setLevelStory(event, user):
 
 def RandomTest(user):
     #global user.test_type_list
-    user.test_type_list = [random.randint(1,7) for _ in range(5)]
+    user.test_type_list = [random.randint(3,3) for _ in range(5)]
     print("-----*** 5 Quiz type = ",user.test_type_list)
 
 def LoadTestIndex(user):
